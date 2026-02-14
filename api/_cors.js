14d9c464fc5424f4f1d757e264ab139825692c85
taqniea21 <taqniea21@gmail.com>
2026-02-14 14:49:22 +0300

    Create _cors.js

---
+++ api/_cors.js
@@ -0,0 +1,17 @@
+export function setCors(req, res) {
+  // Allow your Base44 frontend + local dev.
+  // You can tighten later.
+  const origin = req.headers.origin || "*";
+  res.setHeader("Access-Control-Allow-Origin", origin);
+  res.setHeader("Vary", "Origin");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    res.statusCode = 204;
+    res.end();
+    return true; // handled
+  }
+  return false;
+}
